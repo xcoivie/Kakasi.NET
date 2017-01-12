9709ebe0ab54ae9f149d018f41decaa24f2f3473
xuannt.ext <xuannt.ext@mytrax.co.jp>
2017-01-12 11:00:15 +0700

    first commit test convert from file.

--- Src/Kakasi.NET.TestApp/Program.cs
+++ Src/Kakasi.NET.TestApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Kakasi.NET.Interop;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Kakasi.NET.TestApp
 {
 
@@ -15,43 +16,86 @@ namespace Kakasi.NET.TestApp
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
-        {
-
-            // Initialize library
+        {// Initialize library
             KakasiLib.Init();
 
             // Set params to get Furigana
             // NOTE: Use EUC-JP encoding as the wrapper will encode/decode using it
             KakasiLib.SetParams(new[] { "kakasi", "-ieuc", "-f", "-JH", "-w" });
+            //KakasiLib.SetParams(new[] { "kakasi", "-ieuc", "-w" });
 
             // Get furigana from this book: アンブラとパン先生 by 伊藤 綾子
             // http://itunes.apple.com/us/book/id1144287620
 
             // Set source sentence
-            var value1 = @"この森は昼でも薄暗く、気味が悪いので、村人は誰も近づかないのでした。";
-
+            //
             // Get furigana
-            var resultValue1 = KakasiLib.DoKakasi(value1);
+            int counter = 0;
+            string line;
+            List<string> data = new List<string>();
 
-            // Expected result
-            // この 森[もり] は 昼[ひる] でも 薄暗く[うすぐらく] 、 気味[きみ] が 悪い[わるい] ので 、 村人[むらびと] は 誰も[だれも] 近づ[ちかづ] かないのでした 。
-            Debug.Assert(resultValue1 == @"この 森[もり] は 昼[ひる] でも 薄暗く[うすぐらく] 、 気味[きみ] が 悪い[わるい] ので 、 村人[むらびと] は 誰も[だれも] 近づ[ちかづ] かないのでした 。");
+            // Read the file and display it line by line.
+            using (System.IO.StreamReader file =
+               new System.IO.StreamReader(@"D:\temp\queryUpdateForSearchTag_9_shorter.sql"))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    var resultValue2 = System.Text.RegularExpressions.Regex.Replace(line, @"[％：、。○★『』◯・（）「」／＆！＋？《》°…’＜＞〈〉【】～〜×❌!@#$%^&*()_\+={}\\|:;'<>?]", " ");
+                    resultValue2 = resultValue2.ToLower().Replace("　", " ").Replace("  ", " ").Replace(" ", ",").Replace(",,", ",");
+                    //System.Text.RegularExpressions.Regex.Replace(resultValue2, @"[％：、。○★『』◯・（）「」／＆！＋？《》°…’＜＞〈〉【】～〜×❌!@#$%^&*()_\+={}\\|:;'<>?]", " ");
+                    var separatedList2 = resultValue2.Split(new string[] { "," }, System.StringSplitOptions.RemoveEmptyEntries);
+                    var tastedId = separatedList2[0];
+                    if (tastedId == "7pj0bdrqgl3o8")
+                    {
+                        ;
+                    }
+                    separatedList2[0] = string.Empty;
+                    List<string> list2 = new List<string>();
+                    list2.AddRange(separatedList2);
 
-            // Get furigana from this book: プカプカ島の謎 by 伊藤 綾子
-            // http://itunes.apple.com/us/book/id1146306209
+                    // Get furigana
+                    var resultValue1 = KakasiLib.DoKakasi(line.ToLower());
+                    //TODO
+                    resultValue1 = System.Text.RegularExpressions.Regex.Replace(resultValue1, @"[％：、。○★『』◯・（）「」／＆！＋？《》°…’＜＞〈〉【】～〜×❌!@#$%^&*()_\+={}\\|:;'<>?]", " ");
+                    resultValue1 = resultValue1.ToLower().Replace("　", " ").Replace("  ", " ").Replace(" ", ",").Replace(",,", ",");
+                    var separatedList = resultValue1.Split(new string[] { "," }, System.StringSplitOptions.RemoveEmptyEntries);
 
-            // Set source sentence
-            var value2 = @"この島の住人を紹介しましょう";
+                    separatedList[0] = string.Empty;
+                    List<string> distinctSeparatedList = new List<string>();
+                    foreach (var kk in separatedList)
+                    {
+                        var str = kk.Replace(" ", string.Empty).Replace(",,", ",");
+                        if (!string.IsNullOrEmpty(str)
+                            && !distinctSeparatedList.Contains(str)
+                            && !list2.Any(r => r.ToLower().IndexOf(str) == 0)
+                            && !(
+                            str.Length == 1 && !str.Any(r => (r >= 0x4E00 && r <= 0x9FBF)) /* kanji */
+                            )
+                            )
+                        {
+                            distinctSeparatedList.Add(str);
+                        }
+                    }
+                    if (distinctSeparatedList.Count > 0)
+                    {
+                        string newSearchTag = string.Format("update Tasted set SearchTag = SearchTag + N',{0}' where TastedId = '{1}';", string.Join(",", distinctSeparatedList), tastedId);
+                        data.Add(newSearchTag);
+                    }
+                    counter++;
+                }
+            }
 
-            // Get furigana
-            var resultValue2 = KakasiLib.DoKakasi(value2);
+            //Write file            
+            using (System.IO.StreamWriter filew =
+               new System.IO.StreamWriter(@"D:\temp\queryUpdateForSearchTag_9_shorter-converted.sql"))
+            {
+                foreach (string d in data)
+                {
+                    filew.WriteLine(d);
+                }
+            }
 
-            // Expected result
-            // この 島[しま] の 住人[じゅうにん] を 紹介[しょうかい] しましょう
-            Debug.Assert(resultValue2 == @"この 島[しま] の 住人[じゅうにん] を 紹介[しょうかい] しましょう");
-
-            // Dispose of library
-            KakasiLib.Dispose();            
+            KakasiLib.Dispose();
 
         }
 
